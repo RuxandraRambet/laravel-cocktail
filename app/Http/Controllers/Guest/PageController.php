793d81deec9a39151f1457004a29a0bb91a60c2f
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-31 17:24:40 +0100

    feat: visualization cocktails

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cocktail;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-   public function index(){
-    return view('welcome');
+   public function index()
+   {
+
+      $cocktails = Cocktail::all();
+
+      return view('welcome', compact('cocktails'));
    }
 }
