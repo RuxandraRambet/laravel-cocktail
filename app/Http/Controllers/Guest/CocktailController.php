88474ac476f830fdeed0955e5521aeb9d08b68a4
rebs-code <jack.rebussi@gmail.com>
2024-02-08 17:28:37 +0100

    Refactor destroy method in CocktailController

--- app/Http/Controllers/Guest/CocktailController.php
+++ app/Http/Controllers/Guest/CocktailController.php
@@ -74,8 +74,9 @@ class CocktailController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Cocktail $cocktail)
     {
-        //
+        $cocktail->delete();
+        return redirect()->route('cocktails.index');
     }
 }
